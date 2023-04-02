46b292d3d66431842c625cbb147c539caa168d85
Justin Wong <tengwei101@gmail.com>
2023-04-02 22:08:50 +0800

    New Group Chat 2

---
+++ app/src/main/java/com/mobile/groupchat4/DashboardActivity.java
@@ -0,0 +1,122 @@
+package com.mobile.groupchat4;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.mobile.groupchat4.fragment.CharitiesFragment;
+import com.mobile.groupchat4.fragment.ChatListFragment;
+import com.mobile.groupchat4.fragment.GroupChatsFragment;
+import com.mobile.groupchat4.fragment.UsersFragment;
+
+public class DashboardActivity extends AppCompatActivity {
+    FirebaseAuth firebaseAuth;
+    ActionBar actionBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+
+        actionBar = getSupportActionBar();
+        actionBar.setTitle("Profile");
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        //bottom navigation
+        BottomNavigationView navigationView = findViewById(R.id.navigation);
+        navigationView.setOnNavigationItemSelectedListener(selectedListener);
+
+        // default
+        actionBar.setTitle("Users");
+        UsersFragment fragment1 = new UsersFragment();
+        FragmentTransaction ft1 = getSupportFragmentManager().beginTransaction();
+        ft1.replace(R.id.content, fragment1, "");
+        ft1.commit();
+    }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener selectedListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    switch (item.getItemId()){
+                        case R.id.nav_users:
+                            // home fragment transaction
+                            actionBar.setTitle("Users");
+                            UsersFragment fragment1 = new UsersFragment();
+                            FragmentTransaction ft1 = getSupportFragmentManager().beginTransaction();
+                            ft1.replace(R.id.content, fragment1, "");
+                            ft1.commit();
+                            return true;
+                        case R.id.nav_chat:
+                            // home fragment transaction
+                            actionBar.setTitle("Chat");
+                            ChatListFragment fragment2 = new ChatListFragment();
+                            FragmentTransaction ft2 = getSupportFragmentManager().beginTransaction();
+                            ft2.replace(R.id.content, fragment2, "");
+                            ft2.commit();
+                            return true;
+
+                        case R.id.nav_groupChat:
+                            // home fragment transaction
+                            actionBar.setTitle("GroupChat");
+                            GroupChatsFragment fragment3 = new GroupChatsFragment();
+                            FragmentTransaction ft3 = getSupportFragmentManager().beginTransaction();
+                            ft3.replace(R.id.content, fragment3, "");
+                            ft3.commit();
+                            return true;
+                    }
+                    return false;
+                }
+            };
+
+    private void checkUserStatus(){
+        FirebaseUser user = firebaseAuth.getCurrentUser();
+        if (user != null){
+
+            // Name, email address, and profile photo Url
+            String name = user.getDisplayName();
+            String email = user.getEmail();
+            Uri photoUrl = user.getPhotoUrl();
+
+
+            // Check if user's email is verified
+            boolean emailVerified = user.isEmailVerified();
+
+            // The user's ID, unique to the Firebase project. Do NOT use this value to
+            // authenticate with your backend server, if you have one. Use
+            // FirebaseUser.getIdToken() instead.
+            String uid = user.getUid();
+        }
+        else {
+            //user not signed in, go to main Activity
+            startActivity(new Intent(DashboardActivity.this, MainActivity.class));
+            finish();
+        }
+    }
+
+    @Override
+    public void onBackPressed(){
+        //check on start of app
+        super.onBackPressed();
+        finish();
+    }
+
+    @Override
+    protected void onStart(){
+        //check on start of app
+        checkUserStatus();
+        super.onStart();
+    }
+
+
+}
